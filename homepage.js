b91491746b65d4b827815dd63c35b35d98d310bc
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2017-02-11 12:49:40 -0600

    homepage for prototype 1

---
+++ homepage.js
@@ -0,0 +1,4 @@
+var login = document.getElementById("login");
+login.onclick = function(e) {
+  window.open("/Users/nneomaoradiegwu/Documents/School/2016-2017/Winter/EECS 330/HW/1/calculator.html", "_self");
+}
